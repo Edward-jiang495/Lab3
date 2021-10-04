6d086ea397174660f00a452865774de196cb71e3
Edward-jiang495 <54651927+Edward-jiang495@users.noreply.github.com>
2021-10-04 10:53:16 -0500

    basic files

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Zhengran Jiang on 10/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
