afe9c2e6ed1d7415433a252203fa59fb43159e63
Edward-jiang495 <54651927+Edward-jiang495@users.noreply.github.com>
2021-10-04 17:19:31 -0500

    mod ab added

---
+++ Lab3/ModuleBViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ModuleBViewController.swift
+//  Lab3
+//
+//  Created by Zhengran Jiang on 10/4/21.
+//
+
+import UIKit
+
+class ModuleBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
