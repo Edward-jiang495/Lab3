aeddf9b877662b797b1157aef6a810a63aaf2a18
Edward-jiang495 <54651927+Edward-jiang495@users.noreply.github.com>
2021-10-05 18:10:17 -0500

    new interface

--- Lab3/ViewController.swift
+++ Lab3/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pastStepNum = 0;
+    //yesterday's step
+    var currStepNum = 0;
+    //today's step
+    var highScoreNum = 0;
+    //high score
+    var currScoreNum = 0;
+    //current score
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pastStepNum = 0
+        currStepNum = 0
+        highScoreNum = 0
+        currScoreNum = 0
+        pastStep.text = "Yesterday's Step \n \(pastStepNum)"
+        currStep.text = "Today's Step \n \(currStepNum)"
+        highScore.text = "High Score \n \(highScoreNum)"
+        currScore.text = "Current Score \n \(currScoreNum)"
     }
+    
 
+    @IBOutlet weak var pastStep: UILabel!
+    //yesterday step label
+    @IBOutlet weak var currStep: UILabel!
+    //today's step label
+    @IBOutlet weak var highScore: UILabel!
+    //high score label
+    @IBOutlet weak var currScore: UILabel!
+    //current score label
+    
+    
+    @IBOutlet weak var startGame: UIButton!
+    
+    
+    @IBAction func start(_ sender: UIButton) {
+        if(startGame.titleLabel?.text == "Start"){
+            startGame.setTitle("Restart", for: .normal)
+        }
+        else{
+            startGame.setTitle("Start", for: .normal)
+        }
+    }
+    
 
 }
 
