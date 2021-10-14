ce79dd17fa0cbb5d90ae94fc89221c1ef6c41f4b
Edward-jiang495 <54651927+Edward-jiang495@users.noreply.github.com>
2021-10-14 16:17:04 -0500

    track finished

--- Lab3/GameScene.swift
+++ Lab3/GameScene.swift
@@ -16,15 +16,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let touch = touches.first!
         let location = touch.location(in: self.view)
         
-        self.addSpriteBottle(x: location.x, y: location.y)
+        self.addSpriteBottle(xp: location.x, yp: location.y)
     }
-    func addSpriteBottle(x: CGFloat, y: CGFloat){
+    func addSpriteBottle(xp: CGFloat, yp: CGFloat){
         let spriteA = SKSpriteNode(imageNamed: "matchman.png") // this is
 
         spriteA.size = CGSize(width:size.width*0.05,height:size.height * 0.05)
 
         let randNumber = random(min: CGFloat(0.1), max: CGFloat(0.9))
-        spriteA.position = CGPoint(x: x, y: y)
+        spriteA.position = CGPoint(x: xp, y: yp)
 
         spriteA.physicsBody = SKPhysicsBody(rectangleOf:spriteA.size)
         spriteA.physicsBody?.restitution = random(min: CGFloat(1.0), max: CGFloat(1.5))
@@ -118,9 +118,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         let outerleft = SKSpriteNode()
         let outerright = SKSpriteNode()
-        outerleft.size = CGSize(width:size.width*0.1,height:size.height*0.26)
+        outerleft.size = CGSize(width:size.width*0.1,height:size.height*0.4)
         outerleft.position = CGPoint(x:size.width*0.05, y:size.height*0.38)
-        outerright.size = CGSize(width:size.width*0.1,height:size.height*0.26)
+        outerright.size = CGSize(width:size.width*0.1,height:size.height*0.4)
         outerright.position = CGPoint(x:size.width*0.95, y:size.height*0.38)
         
         
@@ -135,6 +135,50 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             obj.physicsBody?.categoryBitMask = 0x00000001
             self.addChild(obj)
         }
+
+        let rightupperrect = UIBezierPath()
+        rightupperrect.move(to: CGPoint(x:size.width*0.9, y: size.height*0.58))
+        rightupperrect.addLine(to: CGPoint(x:size.width * 0.9, y: size.height * 0.7))
+        rightupperrect.addLine(to: CGPoint(x:size.width * 0.5, y: size.height * 0.7))
+        rightupperrect.close()
+        
+        let leftupperrect = UIBezierPath()
+        leftupperrect.move(to: CGPoint(x:size.width*0.1, y: size.height*0.58))
+        leftupperrect.addLine(to: CGPoint(x:size.width * 0.1, y: size.height * 0.7))
+        leftupperrect.addLine(to: CGPoint(x:size.width * 0.5, y: size.height * 0.7))
+        leftupperrect.close()
+        
+        let rightlowerrect = UIBezierPath()
+        rightlowerrect.move(to: CGPoint(x:size.width*0.9, y: size.height*0.18))
+        rightlowerrect.addLine(to: CGPoint(x:size.width * 0.9, y: size.height * 0.06))
+        rightlowerrect.addLine(to: CGPoint(x:size.width * 0.5, y: size.height * 0.06))
+        rightlowerrect.close()
+        
+        let leftlowerrect = UIBezierPath()
+        leftlowerrect.move(to: CGPoint(x:size.width*0.1, y: size.height*0.18))
+        leftlowerrect.addLine(to: CGPoint(x:size.width * 0.1, y: size.height * 0.06))
+        leftlowerrect.addLine(to: CGPoint(x:size.width * 0.5, y: size.height * 0.06))
+        leftlowerrect.close()
+        
+        
+        
+        
+        for arc in [leftupperrect, rightupperrect, leftlowerrect,rightlowerrect]{
+            
+            let obj = SKShapeNode(path: arc.cgPath)
+            obj.physicsBody = SKPhysicsBody(polygonFrom: arc.cgPath)
+            obj.physicsBody?.isDynamic = true
+            obj.physicsBody?.pinned = true
+            obj.physicsBody?.allowsRotation = false
+            obj.strokeColor = UIColor.red
+            obj.physicsBody?.contactTestBitMask = 0x00000001
+            obj.physicsBody?.collisionBitMask = 0x00000001
+            obj.physicsBody?.categoryBitMask = 0x00000001
+            self.addChild(obj)
+            
+        }
+        
+//        leftupperrect.
         
         
         
@@ -163,10 +207,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //we have to add the background image as an sksprite node
         //since ui image will always be in the front covering everything
-//        let trackimage = SKSpriteNode(imageNamed: "track.jpg")
-//        trackimage.size = CGSize(width:size.width,height:size.height*0.7)
-//        trackimage.position = CGPoint(x:size.width*0.5, y:size.height*0.35)
-//        self.addChild(trackimage)
+        let trackimage = SKSpriteNode(imageNamed: "track.jpg")
+        trackimage.size = CGSize(width:size.width,height:size.height*0.7)
+        trackimage.position = CGPoint(x:size.width*0.5, y:size.height*0.35)
+        self.addChild(trackimage)
 
     }
     
